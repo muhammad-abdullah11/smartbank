abc05b9c0419a70c008d97ba6ddd80045d38a65e
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-05-08 20:52:53 +0500

    add loan model

---
+++ Models/loan.Model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const loanSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    amount: { type: Number, required: true },
+    duration: { type: Number, required: true },
+    reason: { type: String },
+    status: {
+      type: String,
+      enum: ["PENDING", "APPROVED", "REJECTED"],
+      default: "PENDING",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Loan || mongoose.model("Loan", loanSchema);
